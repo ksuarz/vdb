2a7871a02d541236bb262c33a7c4c03a926ab791
Kyle Suarez <ksuarz@gmail.com>
2013-10-13 04:32:14 -0400

    Hooray API is ready

--- vdb.py
+++ vdb.py
@@ -1,57 +1,48 @@
-import subprocess
-import vim
+"""
+vdb.py - The public API for the Vim debugger.
 
-from Queue import Queue, Empty
-from threading import Thread
+Implement these methods and shove your file in the ftplugin folder for your
+language-specific debugger.
+"""
 
-class DBSession():
+class VDBSession():
+    """The abstract API for Vim debugging sessions."""
     def __init__(self):
-        """Initializes a gdb session in the background."""
-        self.p = subprocess.Popen(['gdb'],
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT,
-                                stdin=subprocess.PIPE)
-        self.queue = Queue()
-        self.daemon = Thread(target=self._queue_output)
-        self.daemon.daemon = True
-        self.daemon.start()
-        self.ready = True
-        self.p.stdin.write("set prompt (gdb)\\n\n")
+        pass
 
-    def _queue_output(self):
-        """Code for a daemon that constantly reads from stdout.
+    def break(self, linenumber):
+        """Adds a breakpoint at the given line."""
+        pass
 
-        Reads from stdout, blocking if necessary. If we detect output, enqueue
-        the text into our queue.
-        """
-        for line in iter(self.p.stdout.readline, b''):
-            self.queue.put(line)
-        else:
-            self.p.stdout.write("\n")
-            data = self.p.stdout.readline()
-            if data:
-                self.queue.put(data)
-        self.p.stdout.close()
+    def clear(self, linenumber):
+        """Clears the breakpoint at the given line."""
+        pass
 
-    def read_output(self):
-        """ Reads from stdout of gdb, returning a newline-terminated line or
-        None if nothing is in the output queue.
-        """
-        try:
-            line = self.queue.get_nowait()
-        except Empty:
-            self.ready = True
-            return None
-        else:
-            self.ready = False
-            return line
+    def execute(self, cmd):
+        """Executes the command in the debugger."""
+        pass
 
-    def add_breakpoint(self, linenumber):
-        """Adds a breakpoint at the specified linenumber."""
-        self.p.stdin.write("break %d\n" % linenumber)
-        # TODO callback
-        read()
+    def get_response(self):
+        """Returns a string with the latest debugger output."""
+        pass
 
-    def clear_breakpoint(self, linenumber):
-        self.p.stdin.write("clear %d\n" % linenumber)
-        # TODO callback
+    def next(self):
+        """Runs the next line."""
+        pass
+
+    def quit(self):
+        """Quits the debug session cleanly."""
+        pass
+
+    def run(self, filename):
+        """Runs the debugger."""
+        pass
+
+    def start(self):
+        """Starts a debugging session."""
+        pass
+
+    def step(self):
+        """Steps into the next function call."""
+        pass
+
